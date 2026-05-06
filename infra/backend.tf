b68cb85dec3b87f5c727137563a95cf15f7f2264
danielarell <luis.arellano@iteso.mx>
2026-05-06 13:52:40 -0600

    Keys estado terraform por environment

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "s3" {
     bucket         = "cropsight-tfstate"
-    key            = "cropsight/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "cropsight-lock"
     encrypt        = true
   }
-} 
+}
